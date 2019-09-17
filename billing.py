f7e086c58c82abb3cfdd197b5a7211cfa1cb249c
unknown <stevtymez@gmail.com>
2019-09-17 08:25:49 -0700

    I added the main and add Item window

--- billing.py
+++ billing.py
@@ -6,17 +6,34 @@ except ImportError:
     from tkinter import *
 
 window  = Tk()
-window.geometry("600x600")
+window.geometry("950x800")
 window.title("Restaurant Billing System")
+#=========== global variable for entries ============
+#=========== login variable============
 
 usernameVar = StringVar()
 passwordVar = StringVar()
-def adminLogin():
-    titleLabel = Label(window,text="P&E Billing System")
-    titleLabel.grid(row = 0,column = 0,columnspan = 2,padx = (20,0),pady=(30,0))
 
-    loginLabel = Label(window,text = "Admin Login")
-    loginLabel.grid(row = 1,column = 2,padx = 20,pady = 10)
+
+#=========main window variable=========
+
+options=("shake","Noodles","Burger")
+itemVariable=StringVar()
+itemVariable.set(options[0])
+quantityVar = StringVar()
+
+#========= add item variables======
+addItemNameVar=StringVar()
+addItemRateVar=StringVar()
+addItemTypeVar=StringVar()
+addstoredVar=StringVar()
+
+def adminLogin():
+    titleLabel = Label(window,text="P&E Billing System",font="Arial 40",fg="green")
+    titleLabel.grid(row = 0,column = 0,columnspan = 4,padx = (20,0),pady=(30,0))
+
+    loginLabel = Label(window,text = "Admin Login",font="Arial 40")
+    loginLabel.grid(row = 1,column = 2,padx=(50,0),columnspan=2,pady = 10)
 
     usernameLabel = Label(window,text = "Username")
     usernameLabel.grid(row = 2,column = 2,padx = 20,pady=5)
@@ -33,5 +50,67 @@ def adminLogin():
     loginButton = Button(window,text = "Login",width=20,height = 2)
     loginButton.grid(row = 4,column = 2,columnspan = 2)
 
-adminLogin()    
+def mainwindow():
+     titleLabel = Label(window,text="P&E Billing System",font="Arial 40",fg="green")
+     titleLabel.grid(row = 0,column = 0,columnspan = 3,pady=(30,0))
+
+     addNewItem = Button(window, text="Add Item", width=15, height=2)
+     addNewItem.grid(row=1, column=0, padx=(10,0),pady=(10,0))
+
+     logoutBtn = Button(window, text="Logout",width=15, height=2)
+     logoutBtn.grid(row=1, column=5, padx=(10,0),pady=(10,0))
+
+     itemLabel = Label(window, text="Select Item")
+     itemLabel.grid(row=2, column=0, padx=(5,0),pady=(10,0))
+
+     itemDropDown=OptionMenu(window,itemVariable,*options)
+     itemDropDown.grid(row=2, column=1,padx=(10,0), pady=(10,0))
+
+     quantityLabel = Label(window, text="Quantity")
+     quantityLabel.grid(row=2, column=2,padx=(5,0),pady=(10,0))
+     quantityEntry=Entry(window, textvariable=quantityVar)
+     quantityEntry.grid(row=2, column=3,padx=(5,0),pady=(10,0))
+
+     buttonBill = Button(window, text="Generate Bill", width=15)
+     buttonBill.grid(row=2, column=4,padx=(5,0),pady=(10,0))
+
+def addItem():
+     titleLabel = Label(window,text="P&E Billing System",width=30,font="Arial 40",fg="green")
+     titleLabel.grid(row = 0,column = 0,columnspan = 5,pady=(30,0))
+
+     itemNameLabel = Label(window,text="Name")
+     itemNameLabel.grid(row=1, column=1, pady=(10,0))
+
+     itemNameEntry=Entry(window, textvariable=addItemNameVar)
+     itemNameEntry.grid(row=1,column=2,pady=(10,0))
+
+     itemRateLabel = Label(window,text="Rate")
+     itemRateLabel.grid(row=1, column=3, pady=(10,0))
+
+     itemRateEntry=Entry(window, textvariable=addItemNameVar)
+     itemRateEntry.grid(row=1,column=4,pady=(10,0))
+
+     itemTypeLabel = Label(window,text="Type")
+     itemTypeLabel.grid(row=2, column=1, pady=(10,0))
+
+     itemTypeEntry=Entry(window, textvariable=addItemNameVar)
+     itemTypeEntry.grid(row=2,column=2,pady=(10,0))
+
+     storeTypeLabel = Label(window,text="Stored Type")
+     storeTypeLabel.grid(row=2, column=3, pady=(10,0))
+
+     storeEntry=Entry(window, textvariable=addItemNameVar)
+     storeEntry.grid(row=2,column=4,pady=(10,0))
+
+     AddItemButton = Button(window, text="Add Item", width=20, height=2)
+     AddItemButton.grid(row=3, column=3,pady=(10,0))
+
+
+
+
+
+
+
+
+addItem()    
 window.mainloop()
